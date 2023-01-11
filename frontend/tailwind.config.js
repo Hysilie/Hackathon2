b5ae08043fd1ae3940ac0c16676aac5887423c40
MaddieThms <[maddie.thms@gmail.com] mailto:maddie.thms@gmail.com>
2023-01-11 23:48:05 +0100

    add Date Picker

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         "main-yellow": "#FF9900",
         "second-yellow": "#ff8800",
       },
+      dropShadow: {
+        "3xl": "0 8px 8px rgba(0, 0, 0, 1)",
+      },
     },
   },
   plugins: [],
