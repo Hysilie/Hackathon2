2e1b90f49974652a957a10cbf100fe4c2d3d7b68
MaddieThms <[maddie.thms@gmail.com] mailto:maddie.thms@gmail.com>
2023-01-11 16:37:11 +0100

    frontend routes ajusted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import "./index.css";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ForgottenPassword from "./pages/user/ForgottenPassword";
 import Registration from "./pages/user/Registration";
 import Login from "./pages/user/Login";
 import Error from "./pages/Error";
 import MyProfile from "./pages/user/MyProfile";
 import Home from "./pages/Home";
-import { useCurrentUserContext } from "./contexts/UserContext";
 import SearchResults from "./pages/user/SearchResults";
 import ResultDetails from "./pages/user/ResultDetails";
 import Dashboard from "./pages/admin/Dashboard";
@@ -15,29 +14,31 @@ import VehiculeProfile from "./pages/admin/VehiculeProfile";
 import AgencyProfile from "./pages/admin/AgencyProfile";
 
 function App() {
-  const { token } = useCurrentUserContext();
+  // To be used later : const { token } = useCurrentUserContext();
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* </Routes>
       {token ? (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/my-profile" element={<MyProfile />} />
-          <Route Path="/cars" element={<SearchResults />} />
-          <Route Path="/cars/:id" element={<ResultDetails />} />
-          <Route Path="/admin/dashboard" element={<Dashboard />} />
-          <Route Path="/admin/vehicles" element={<AllVehicules />} />
-          <Route Path="/admin/vehicles/:id" element={<VehiculeProfile />} />
-          <Route Path="/admin/agencies/:id" element={<AgencyProfile />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Routes> */}
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route Path="/cars" element={<SearchResults />} />
+        <Route Path="/cars/:id" element={<ResultDetails />} />
+        <Route Path="/admin/dashboard" element={<Dashboard />} />
+        <Route Path="/admin/vehicles" element={<AllVehicules />} />
+        <Route Path="/admin/vehicles/:id" element={<VehiculeProfile />} />
+        <Route Path="/admin/agencies/:id" element={<AgencyProfile />} />
+        <Route path="*" element={<Error />} />
+        {/* </Routes>
       ) : (
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/forgotten-password" element={<ForgottenPassword />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      )}
+        <Routes> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/forgotten-password" element={<ForgottenPassword />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      {/* )} */}
     </div>
   );
 }
