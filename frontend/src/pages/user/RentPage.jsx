e2d253133de8d1e2e4b8c02c8bec2454ac2a18f0
Marion Lalonde <lalondemarion.perso@gmail.com>
2023-01-12 22:30:18 +0100

    Rent OP

--- frontend/src/pages/user/RentPage.jsx
+++ frontend/src/pages/user/RentPage.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+/*eslint-disable*/
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import Bill from "@components/user/Bill";
 import forestBackground from "../../assets/forest-background.jpg";
 import { useCurrentUserContext } from "../../contexts/UserContext";
 import teslaS from "../../assets/teslamodelS.svg";
@@ -7,6 +10,14 @@ import teslaS from "../../assets/teslamodelS.svg";
 export default function RentPage() {
   const navigate = useNavigate();
   const { user } = useCurrentUserContext();
+  const [showModal, setShowModal] = useState(false);
+  const [showBill, setShowBill] = useState(false);
+  const [processing, setProcessing] = useState(false);
+  const submitInformations = (e) => {
+    e.preventDefault();
+    setProcessing(true);
+    setTimeout(() => setShowBill(true), 1000);
+  };
 
   return (
     <section>
@@ -100,11 +111,99 @@ export default function RentPage() {
                 </ul>
               </div>
             </div>
+
+            {/*  */}
+            {processing && showBill ? (
+              <button
+                className="bg-main-yellow text-white active:bg-second-yellow font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => setShowModal(true)}
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  stroke="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                >
+                  <path d="M24 23h-24v-12h4v-10h11.328l4.672 5.031v4.969h4v12zm-20-11h-3v10h22v-10h-3v5h-16v-5zm10-10h-9v14h14v-9h-5v-5zm2 12h-8v-1h8v1zm0-2h-8v-1h8v1zm0-2h-8v-1h8v1zm-1-7.751v3.751h3.573l-3.573-3.751z" />
+                </svg>{" "}
+              </button>
+            ) : processing ? (
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-orange-400"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            ) : null}
+
+            {showModal ? (
+              <>
+                <div className=" justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0  z-50 outline-none focus:outline-none">
+                  <div className=" h-full relative w-auto my-6 mx-auto max-w-1xl">
+                    {/* content */}
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                      {/* header */}
+                      <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                        <h3 className="text-3xl font-semibold">
+                          Your rental invoice
+                        </h3>
+                        <button
+                          className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                          onClick={() => setShowModal(false)}
+                        >
+                          <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                            ×
+                          </span>
+                        </button>
+                      </div>
+                      {/* body */}
+                      <div className="relative p-6 flex-auto">
+                        <Bill />
+                      </div>
+                      {/* footer */}
+                      <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                        <button
+                          className="text-main-yellow background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={() => setShowModal(false)}
+                        >
+                          Close
+                        </button>
+                        <button
+                          className="text-main-yellow active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={() => print()}
+                        >
+                          Print
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="opacity-90 fixed inset-[-100px] z-40 bg-white" />
+              </>
+            ) : null}
           </div>
         </div>
 
         <div
-          className="py-12  md:py-12 "
+          className="py-12  md:py-6 "
           style={{
             backgroundImage: `url('${forestBackground}')`,
             backgroundSize: "cover",
@@ -112,7 +211,7 @@ export default function RentPage() {
             backgroundPosition: "center",
           }}
         >
-          <h2 className="text-4xl text-white  text-center tracking-tight m-2">
+          <h2 className="text-4xl text-white  text-center tracking-tight ">
             Check your informations
           </h2>
           <div className=" text-center text-sm text-white underline">
@@ -249,6 +348,7 @@ export default function RentPage() {
 
               <div className="col-span-6">
                 <button
+                  onClick={submitInformations}
                   type="button"
                   className="appearance-none block w-full bg-main-yellow text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-second-yellow focus:outline-none focus:bg-second-yellow focus:border-gray-500"
                 >
