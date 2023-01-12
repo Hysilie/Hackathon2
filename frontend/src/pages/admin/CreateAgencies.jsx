74bc67ebdcd0dffcbbf1a999b9f17e41f0c4d200
Jonathan Garonian <j.garonian@gmail.com>
2023-01-12 15:06:49 +0100

    add agencie ok

--- frontend/src/pages/admin/CreateAgencies.jsx
+++ frontend/src/pages/admin/CreateAgencies.jsx
@@ -1,70 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import toast, { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import buildingIcon from "../../assets/building.png";
 
 function CreateVehicule() {
   const navigate = useNavigate();
 
-  const [nameAgencie, setNameAgencie] = useState("");
+  const [name, setNameAgencie] = useState("");
   const [country, setCountry] = useState("");
   const [adress, setAdress] = useState("");
+  const [city, setCity] = useState("");
   const [codePostal, setCodePostal] = useState("");
-  const [lattitude, setLattitude] = useState("");
-  const [location] = useState(`${country + adress + codePostal}`);
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [location, setLocation] = useState("");
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
+  // for alert notification error edit decision after submit
+  const notify = () =>
+    toast.error(
+      "Une erreure est survenue, veuillez vérifier que vous avez bien rempli tous les champs"
+    );
 
-  //   const myHeaders = new Headers();
-  //   myHeaders.append("Authorization", `Bearer ${token}`);
-  //   myHeaders.append("Content-Type", "application/json");
-
-  //   const raw = JSON.stringify({
-  //     title,
-  //     content,
-  //     impact,
-  //     risk,
-  //     benefits,
-  //     date_decision_creation: dateConvertedToSqlFormat(Date.now()),
-  //     date_decision_conflict: dateConvertedToSqlFormat(date_decision_conflict),
-  //     status_decision: "En cours",
-  //     user_id: user.id,
-  //     person_expert: choosePersonExpert,
-  //     person_concern: choosePersonConcern,
-  //   });
-  //   toast
-  //     .promise(
-  //       fetch("http://localhost:5000/decision", {
-  //         method: "POST",
-  //         redirect: "follow",
-  //         body: raw,
-  //         headers: myHeaders,
-  //       }),
-  //       {
-  //         loading: "Envoi en cours",
-  //         success: "Décision envoyée",
-  //         error:
-  //           "Une erreur sur le serveur est survenue lors de l'envoi de la décision",
-  //       }
-  //     )
-  //     .then((response) => {
-  //       response.json();
-  //       console.warn(response.status);
-  //       if (response.status === 201) {
-  //         setTimeout(() => {
-  //           navigate("/home");
-  //         }, 2000);
-  //       } else {
-  //         notify();
-  //       }
-  //     })
-  //     .then((result) => console.warn(result))
-  //     .catch((error) => console.warn("error", error));
-  // };
-
-  // https://api.geoapify.com/v1/geocode/autocomplete?text=Mosco&apiKey=8c400060358842b4a9dd7ddd7b41bc8f
-
+  // fetch to get latitude and logitude by adress
   useEffect(() => {
     const myHeader = new Headers();
 
@@ -73,20 +30,66 @@ function CreateVehicule() {
     };
 
     fetch(
-      `https://api.geoapify.com/v1/geocode/autocomplete?text=${location}&apiKey=8c400060358842b4a9dd7ddd7b41bc8f`,
+      `https://api.geoapify.com/v1/geocode/autocomplete?text=${country}${adress}${city}${codePostal}&apiKey=8c400060358842b4a9dd7ddd7b41bc8f`,
       requestOptions
     )
       .then((response) => response.json())
-      .then((result) => setLattitude(result))
+      .then((result) => {
+        setLatitude(result.features[0].properties.lat);
+        setLongitude(result.features[0].properties.lon);
+        setLocation(result.features[0].properties.formatted);
+      })
       .catch((error) => console.warn("error", error));
-  }, [location]);
+  }, [adress, codePostal, country]);
 
-  console.warn("location", location);
+  // fetch to create agencie
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  console.warn("lattitude", lattitude);
+    const myHeaders = new Headers();
+    // myHeaders.append("Authorization", `Bearer ${token}`);
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      name,
+      location,
+      latitude,
+      longitude,
+    });
+
+    toast
+      .promise(
+        fetch("http://localhost:5000/agencie-register", {
+          method: "POST",
+          redirect: "follow",
+          body: raw,
+          headers: myHeaders,
+        }),
+        {
+          loading: "Envoi en cours",
+          success: "Creation de l'agence envoyée",
+          error:
+            "Une erreur sur le serveur est survenue lors de l'envoi de la décision",
+        }
+      )
+      .then((response) => {
+        response.json();
+        console.warn(response.status);
+        if (response.status === 201) {
+          setTimeout(() => {
+            navigate("/");
+          }, 2000);
+        } else {
+          notify();
+        }
+      })
+      .then((result) => console.warn(result))
+      .catch((error) => console.warn("error", error));
+  };
 
   return (
     <section className="h-auto relative">
+      <Toaster position="top-center" reverseOrder={false} />
       <button
         onClick={() => navigate(-1)}
         type="button"
@@ -117,7 +120,7 @@ function CreateVehicule() {
         </div>
         <div className="flex justify-center my-2 mx-4 md:mx-0">
           <form
-            // onSubmit={handleSubmit}
+            onSubmit={handleSubmit}
             className="w-full max-w-xl bg-white rounded-lg shadow-md p-6"
           >
             <div className="flex flex-col -mx-3 mb-6">
@@ -134,7 +137,7 @@ function CreateVehicule() {
                   id="model"
                   required
                   placeholder="My agencie"
-                  value={nameAgencie}
+                  value={name}
                   onChange={(e) => setNameAgencie(e.target.value)}
                 />
               </div>
@@ -164,8 +167,17 @@ function CreateVehicule() {
                   onChange={(e) => setAdress(e.target.value)}
                 />
                 <input
+                  className="appearance-none mb-2 block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
+                  type="number"
+                  placeholder="City"
+                  required
+                  value={city}
+                  onBlur={(e) => setCity(e.target.value)}
+                  onChange={(e) => setCity(e.target.value)}
+                />
+                <input
                   className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
-                  type="text"
+                  type="number"
                   placeholder="Postal code"
                   required
                   value={codePostal}
